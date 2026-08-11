750de1f6e1e966bae7d47fab4c8fcff3a1e9b1b0
Mohamed shaheem <mohamedshaheem0123@gmail.com>
2026-08-11 22:37:28 +0530

    Add files via upload

---
+++ StudentRecordSystem.py
@@ -0,0 +1,16 @@
+student = {
+    "roll_no": 12343,
+    "name": "sha",
+    "course": "cse"
+}
+
+print(student)
+
+student["mark"] = 98
+
+print("After adding:", student)
+student["mark"]=99
+print("after modify:",student)
+
+student["remark"]="no"
+print(student)
